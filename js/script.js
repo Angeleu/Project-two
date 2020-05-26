67726784c0a8728760b44b61c9dcb3fc48b7d706
Angeleu <54134462+Angeleu@users.noreply.github.com>
2020-05-26 16:54:36 -0700

    final updates

--- js/script.js
+++ js/script.js
@@ -2,47 +2,25 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+/** Creating global variables */
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-/** CREATING A VARIABLE studentsList TO SELECT ALL STUDENTS ELEMENTS */
+/** Creating a variable studentsList to select all students elements */
 const studentsList = document.querySelector('.student-list');
 console.log(studentsList);
 
-/** CREATING A VARIABLE studentItems TO STORE THE SELECTED LIST OF ITEMS AND THEIR VALUE  */
+/** Creating a variable studentItems to store the selected list of items and their values */
 const studentItems = studentsList.children;
 const studentsPerPage = 10;
 const page = document.querySelector('.page');
 /*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
+   Creating the `showPage`function to hide the rest of the items in the list of students and show the 10 per page we want to show
 ***/
 const showPage = (studentItems, page) => {
 
    let startIndex = (page * 10) - 10;
    let endIndex = page * 10;
-   /** LOOP OVER STUDENT ITEMS TO DISPLAY THE RIGHT AMOUNT OF ITEMS PER PAGE(10 ITEMS) AND HIDE THE EXCES   */
+
+/** Loop over the list of students to display the 10 per page and hide the rest */
    for (let i = 0; i < studentItems.length; i++) {
       if (i >= startIndex && i < endIndex) {
          studentItems[i].style.display = '';
@@ -51,16 +29,16 @@ const showPage = (studentItems, page) => {
        }
    }
 };
-/*** 
+/** 
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
 const appendPageLinks = (studentItems) => {
-/** ROUNDING UP TO THE HIGHEST INTEGER TO GET THE TOTAL NUMBER OF PAGES WE NEED FOR OUR LIST OF 54 STUDENTS 
- WITH 10 ITEMS PER PAGE  */
+/** Rounding up to the highest integer to get the total number of pages we need for our list of 54 students with 10 students per page
+  */
    let pageNumber = Math.ceil(studentItems.length/studentsPerPage);
 
-/** CREATING A DIV ELEMENT WITH A CLASS NAMED PAGINATION THAT CONTAINS AN UNORDERED LIST OF PAGINATION LINKS */
+/** Creating a div element with a class named `pagination` that contains an unordered list of pagination links*/
    const div = document.createElement('div');
    div.className = 'pagination';
    page.appendChild(div);
@@ -71,15 +49,14 @@ const appendPageLinks = (studentItems) => {
          const a = document.createElement('a');
          a.textContent = i;
 
-      /** GIVE AN ACTIVE CLASS TO THE FIRST PAGINATION LINK */
+      /** Give an active class to the first pagination link */
          if(i === 1) {a.className = "active"}
          ul.appendChild(li);
          li.appendChild(a);
-
+      /** add an event listener that responds to clicked pagination links */
       a.addEventListener('click', (e) => {
    
-      /** LOOP OVER PAGINATION LINKS TO MAKE ONLY THE CLICKED PAGE ACTIVE AND DISPLAY THE SAID PAGE
-      ***/
+      /** Loop over pagination links to make only the clicked link active and display the corresponding page */
          let paginationLinks = document.querySelectorAll('.pagination ul li a');
          for(i=0; i < paginationLinks.length; i++) {
             paginationLinks[i].className = '';
@@ -89,17 +66,18 @@ const appendPageLinks = (studentItems) => {
          showPage(studentItems,a.textContent);
          }
       });
-      /** HIGHLIGHT THE LINK THAT IS POINTED OVER BY THE CURSOR */
-
+      /** Highlight the link that is pointed over with the cursor for better presentation*/
       a.addEventListener('mouseover', (e) => {
          e.target.style.cursor = 'pointer';
       });
       }; 
 
 };
-showPage(studentItems,1); /*** CALLING showPage TO PRINT THE FIRST PAGE */
+   /*** Calling the `showPage` function to print the first page */
+
+showPage(studentItems,1); 
+
 appendPageLinks(studentItems);
 
 
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
